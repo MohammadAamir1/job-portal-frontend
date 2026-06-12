0f8b4ff562d8fd6bd4f3fa32e64bff28cbcd1ca2
mohdamircode <amir113918amir@gmail.com>
2026-06-12 08:39:02 -0700

    Build Signup Page

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,9 +2,10 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Button } from "../ui/button";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { LogOut, User2 } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const user = true;
+  const user = false;
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
@@ -21,10 +22,10 @@ const Navbar = () => {
           </ul>
           {!user ? (
             <div className="flex items-center gap-2">
-              <Button variant="outline">Login</Button>
-              <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">
+              <Link to="/login"><Button variant="outline">Login</Button></Link>
+              <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">
                 Signup
-              </Button>
+              </Button></Link>
             </div>
           ) : (
             <Popover>
@@ -37,28 +38,30 @@ const Navbar = () => {
                 </Avatar>
               </PopoverTrigger>
               <PopoverContent className="w-80">
-                <div className="flex gap-5 space-y-2">
-                  <Avatar className="cursor-pointer">
-                    <AvatarImage
-                      src="https://github.com/shadcn.png"
-                      alt="@shadcn"
-                    />
-                  </Avatar>
-                  <div>
-                    <h4 className="font-medium">Aamir MernStack</h4>
-                    <p className="text-sm text-muted-foreground">
-                      Full Stack Developer
-                    </p>
+                <div className=''>
+                  <div className="flex gap-2 space-y-2">
+                    <Avatar className="cursor-pointer">
+                      <AvatarImage
+                        src="https://github.com/shadcn.png"
+                        alt="@shadcn"
+                      />
+                    </Avatar>
+                    <div>
+                      <h4 className="font-medium">Aamir MernStack</h4>
+                      <p className="text-sm text-muted-foreground">
+                        Full Stack Developer
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-col my-2 gap-3 text-gray-600">
-                  <div className="flex w-fit items-center gap-2 cursor-pointer">
-                    <User2 />
-                    <Button variant="outline">View Profile</Button>
-                  </div>
-                  <div className="flex w-fit items-center gap-2 cursor-pointer">
-                    <LogOut />
-                    <Button variant="outline">Logout</Button>
+                  <div className="flex flex-col my-2 gap-3 text-gray-600">
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <User2 />
+                      <Button variant="link">View Profile</Button>
+                    </div>
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <LogOut />
+                      <Button variant="link">Logout</Button>
+                    </div>
                   </div>
                 </div>
               </PopoverContent>
