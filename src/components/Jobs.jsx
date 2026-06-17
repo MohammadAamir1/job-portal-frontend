4f79ec0bff34d7806a1dc2a08443cec02a2116e1
mohdamircode <amir113918amir@gmail.com>
2026-06-17 10:04:55 -0700

    implement apply job feature,persisting our store

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,36 +1,55 @@
-import { useSelector } from "react-redux";
-import FilterCard from "./FilterCard";
-import Job from "./Job";
-import Navbar from "./shared/Navbar";
 
-const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
-const Jobs = () => {
-  const { allJobs } = useSelector(store => store.job);
-  return (
-    <div>
-      <Navbar />
-      <div className="max-w-7xl mx-auto mt-5">
-        <div className="flex gap-5">
-          <div className="w-20%">
-            <FilterCard />
-          </div>
-          {jobsArray.length <= 0 ? (
-            <span>Job not found</span>
-          ) : (
-            <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
-              <div className="grid grid-cols-3 gap-4">
-                {allJobs.map((job) => (
-                  <div key={job?._id}>
-                    <Job job={job}/>
-                  </div>
-                ))}
-              </div>
+import { Button } from './ui/button'
+import { Bookmark } from 'lucide-react'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { Badge } from './ui/badge'
+import { useNavigate } from 'react-router-dom'
+
+const Job = ({job}) => {
+    const navigate = useNavigate();
+    // const jobId = "lsekdhjgdsnfvsdkjf";
+
+    const daysAgoFunction = (mongodbTime) => {
+        const createdAt = new Date(mongodbTime);
+        const currentTime = new Date();
+        const timeDifference = currentTime - createdAt;
+        return Math.floor(timeDifference/(1000*24*60*60));
+    }
+    
+    return (
+        <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
+            <div className='flex items-center justify-between'>
+                <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) === 0 ? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
+                <Button variant="outline" className="rounded-full" size="icon"><Bookmark /></Button>
             </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
 
-export default Jobs;
+            <div className='flex items-center gap-2 my-2'>
+                <Button className="p-6" variant="outline" size="icon">
+                    <Avatar>
+                        <AvatarImage src={job?.company?.logo} />
+                    </Avatar>
+                </Button>
+                <div>
+                    <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
+                    <p className='text-sm text-gray-500'>India</p>
+                </div>
+            </div>
+
+            <div>
+                <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
+                <p className='text-sm text-gray-600'>{job?.description}</p>
+            </div>
+            <div className='flex items-center gap-2 mt-4'>
+                <Badge className={'text-blue-700 font-bold'} variant="ghost">{job?.position} Positions</Badge>
+                <Badge className={'text-[#F83002] font-bold'} variant="ghost">{job?.jobType}</Badge>
+                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">{job?.salary}LPA</Badge>
+            </div>
+            <div className='flex items-center gap-4 mt-4'>
+                <Button onClick={()=> navigate(`/description/${job?._id}`)} variant="outline">Details</Button>
+                <Button className="bg-[#7209b7]">Save For Later</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Job
